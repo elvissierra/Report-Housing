3d54accff7e92f84824281ec8ae08b557bcfccf6
Elvis Sierra <elvissierra3@gmail.com>
2025-12-08 13:30:35 -0600

    rm feedback link for now

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -25,7 +25,7 @@
         target="_blank"
         :disabled="FEEDBACK_LINK === '#'"
       >
-        Feedback (Quip)
+        Feedback (TBD)
       </v-btn>
 
       <input ref="fileInput" type="file" accept="application/json" class="d-none" @change="onImport" />
