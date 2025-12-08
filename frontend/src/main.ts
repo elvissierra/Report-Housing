aa7646cca7844ac24adab892106e6e05244b58bc
Elvis Sierra <elvissierra3@gmail.com>
2025-12-08 16:25:19 -0600

    vuetify/styles/main.css import

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
-import 'vuetify/styles'
+import 'vuetify/styles/main.css'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
